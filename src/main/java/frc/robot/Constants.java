ce8a72be8c342ec38aa23111e8e3f58955f2a08a
Guadalupe Murgo <guadazoemurgo@gmail.com>
2025-01-14 09:57:48 -0300

    Agregue el subsistema de movimiento lo hice guiandome de el codigo del kitbot y cambiando los nombres de algunas cosas para que fuera mas facil de entender Agregue Constants y dentro los motores y el joystick

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,16 @@
+
+package frc.robot;
+
+public final class Constants {
+  public static final class DriveConstants {
+    public static final int MotorMovimientoIzquierdoLider_ID = 1;
+    public static final int MotorMovimientoIzquierdoSeguidor_ID = 2;
+    public static final int MotorMovimientoDerechoLider_ID = 3;
+    public static final int MotorMovimientoDerechoSeguidor_ID = 4;
+
+    public static final int DRIVE_MOTOR_CURRENT_LIMIT = 60;
+  }
+  public static final class OperatorConstants {
+    public static final int DRIVER_CONTROLLER_PORT = 0;
+  }
+}
---
+++ src/main/java/frc/robot/commands/MovimientoCommand.java
@@ -0,0 +1,45 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.MovimientoSubsystem;
+import java.util.function.DoubleSupplier;
+
+public class MovimientoCommand extends Command {
+  private final DoubleSupplier xSpeed;
+  private final DoubleSupplier zRotation;
+  private final MovimientoSubsystem driveSubsystem;
+
+  public MovimientoCommand(
+      DoubleSupplier xSpeed, DoubleSupplier zRotation, MovimientoSubsystem driveSubsystem) {
+    this.xSpeed = xSpeed;
+    this.zRotation = zRotation;
+    this.driveSubsystem = driveSubsystem;
+
+    addRequirements(this.driveSubsystem);
+  }
+
+  // Runs each time the command is scheduled.
+  @Override
+  public void initialize() {
+  }
+
+  // Runs every cycle while the command is scheduled (~50 times per second)
+  @Override
+  public void execute() {
+    driveSubsystem.driveArcade(xSpeed.getAsDouble(), zRotation.getAsDouble());
+  }
+
+  // Runs each time the command ends via isFinished or being interrupted.
+  @Override
+  public void end(boolean isInterrupted) {
+  }
+
+  // Runs every cycle while the command is scheduled to check if the command is
+  // finished
+  @Override
+  public boolean isFinished() {
+    // Return false to indicate that this command never ends. It can be interrupted
+    // by another command needing the same subsystem.
+    return false;
+  }
+}
