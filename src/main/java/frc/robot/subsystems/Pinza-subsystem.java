85d8c51c7cdcb5907a671513de100313e14452d8
Fran <mercedesperazz@gmail.com>
2025-01-14 11:47:36 -0300

    Creacion de la clase para el funcionamiento de la pinza, y cambio del nombre del archivo para mayor orden/division.

---
+++ src/main/java/frc/robot/subsystems/Pinza-subsystem.java
@@ -0,0 +1,17 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.motorcontrol.Spark;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj.Joystick;
+
+public class PinzaSubsystem extends SubsystemBase{
+    public boolean estado(){
+
+    }
+    public void giro(){
+
+    }   
+    public void rodillos(){
+
+    }
+}
