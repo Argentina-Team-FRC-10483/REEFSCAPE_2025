832c4d9933b70c06f370fcd65e1026d72a68e49a
Jeremías <rjspyro976@gmail.com>
2025-01-27 09:52:25 -0300

    AlgaeIntakeSubsystem ahora tiene Neos Brushless

--- src/main/java/frc/robot/subsystems/AlgaeIntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/AlgaeIntakeSubsystem.java
@@ -14,8 +14,8 @@ public class AlgaeIntakeSubsystem extends SubsystemBase {
     private final SparkMax brazoRodilloMotor;
 
     public AlgaeIntakeSubsystem(){
-        rodilloMotor = new SparkMax(AlgaeIntakeConstants.RodilloMotor_ID, MotorType.kBrushed);
-        brazoRodilloMotor = new SparkMax(AlgaeIntakeConstants.brazoRodilloMotor_ID, MotorType.kBrushed);
+        rodilloMotor = new SparkMax(AlgaeIntakeConstants.RodilloMotor_ID, MotorType.kBrushless);
+        brazoRodilloMotor = new SparkMax(AlgaeIntakeConstants.brazoRodilloMotor_ID, MotorType.kBrushless);
         SparkMaxConfig rodilloMotorConfig = new SparkMaxConfig();
         rodilloMotorConfig.voltageCompensation(AlgaeIntakeConstants.RodilloMotor_CompVolt);
         rodilloMotorConfig.smartCurrentLimit(AlgaeIntakeConstants.RodilloMotor_LIMITE);
