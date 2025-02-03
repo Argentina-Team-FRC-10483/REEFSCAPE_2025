a284b062f2e0fb14afeaefbb11ac8f405150f155
Fran <mercedesperazz@gmail.com>
2025-02-03 11:10:29 -0300

    Se agregaron las configuraciones basicas para el funcionamiento de los motores NEO.

--- src/main/java/frc/robot/subsystems/ArmSubsystem.java
+++ src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -1,6 +1,12 @@
 package frc.robot.subsystems;
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.config.SparkMaxConfig;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+import com.revrobotics.AbsoluteEncoder;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
 import frc.robot.Constants.ArmConstants;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
@@ -8,12 +14,30 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 public class ArmSubsystem extends SubsystemBase{
 
     private final SparkMax motorShoulder;
+    private final AbsoluteEncoder encoder;
+    private long debugTimeArmSub = 0;
 
-public ArmSubsystem(){
-    this.motorShoulder = new SparkMax(ArmConstants.ArmMotorShoulder_ID, MotorType.kBrushless);
-}
+    public ArmSubsystem(){
+        //Motor
+        this.motorShoulder = new SparkMax(ArmConstants.ArmMotorShoulder_ID, MotorType.kBrushless);
+        // Encoder
+        this.encoder = motorShoulder.getAbsoluteEncoder(); 
+        double position = encoder.getPosition();
+        double pulsos = position * 42;
+        double velocidad = encoder.getVelocity();
+        //Configuraciones
+        SparkMaxConfig config = new SparkMaxConfig();
+        config.idleMode(IdleMode.kBrake);
+        config.smartCurrentLimit(40, 1,5676);
+        config.voltageCompensation(12);
+    }
 
-    public void armMove(double angle){
-        motorShoulder.set(angle);
+    @Override
+    public void periodic(){
+        SmartDashboard.putNumber("Motor Brazo", motorShoulder.get());
+    }
+
+    public void armMove(double torque){
+        motorShoulder.set(torque);
     }
 }
--- src/main/java/frc/robot/subsystems/ClawSubsystem.java
+++ src/main/java/frc/robot/subsystems/ClawSubsystem.java
@@ -2,6 +2,9 @@ package frc.robot.subsystems;
 
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.config.SparkBaseConfig;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+import com.revrobotics.spark.config.SparkMaxConfig;
 
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -16,8 +19,14 @@ public class ClawSubsystem extends SubsystemBase{
     private long debugTimeClawSub = 0;
 
     public ClawSubsystem(){
+        //Motores
         this.motorCenter = new SparkMax(PinzaConstants.PinzaMotorRodilloCentral_ID, MotorType.kBrushless);
         this.motorRight = new SparkMax(PinzaConstants.PinzaMotorRodilloDerecho_ID, MotorType.kBrushless);
+        //Configuraciones
+        SparkMaxConfig config = new SparkMaxConfig();
+        config.idleMode(IdleMode.kBrake);
+        config.smartCurrentLimit(30, 1, 11000 );
+        config.voltageCompensation(12);
     }
 
     @Override
