8bbd6415ea5df7c9971652828aaa4430d938b6c1
Fran <mercedesperazz@gmail.com>
2025-02-04 08:21:53 -0300

    Se corriguio el or por un and para la aceleracion y se restauraron los if de velocidad y giro

--- src/main/java/frc/robot/commands/MovimientoCommand.java
+++ src/main/java/frc/robot/commands/MovimientoCommand.java
@@ -50,7 +50,7 @@ public class MovimientoCommand extends Command {
 
     double deltaTime = Timer.getFPGATimestamp() - lastTime;
 
-    if(((velocidad != 0.5) && (velocidad != -0.5)) || ((giro != 0.5) && (giro != -0.5))){
+    if(((velocidad != 0.5) && (velocidad != -0.5)) && ((giro != 0.5) && (giro != -0.5))){
       this.acel = 0;
     }
     else if((bumper.getAsBoolean() && this.acel != 0.5)) {
@@ -71,9 +71,17 @@ public class MovimientoCommand extends Command {
     SmartDashboard.putNumber("Aceleracion", acel);
     SmartDashboard.putNumber("Velocidad Total", velocidad+acel);
 
-    velocidad = (velocidad != 0) ? velocidad - acel : velocidad + acel;
-
-    giro = (giro != 0) ? giro - acel : giro + acel;
+    if (velocidad > 0){
+      velocidad = velocidad + acel;
+    } else if (velocidad != 0){
+      velocidad = velocidad - acel;
+    }
+    
+    if (giro > 0){
+      giro = giro + acel;
+    } else if (giro != 0){
+      giro = giro - acel;
+    }
 
     this.lastTime = Timer.getFPGATimestamp();
 
