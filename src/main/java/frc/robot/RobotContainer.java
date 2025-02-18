feef564f6a5199dea62a48bd43dacaf649569160
Joaquín Figueroa <ijfigueroa2007@gmail.com>
2025-02-18 11:56:49 -0300

    Se cambio el metodo de mandar el comando

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -19,9 +19,12 @@ import frc.robot.subsystems.EngancheSubsystem;
 import frc.robot.subsystems.MovimientoSubsystem;
 
 /**
- * This class is where the bulk of the robot should be declared. Since Command-based is a
- * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
- * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
+ * This class is where the bulk of the robot should be declared. Since
+ * Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in
+ * the {@link Robot}
+ * periodic methods (other than the scheduler calls). Instead, the structure of
+ * the robot (including
  * subsystems, commands, and trigger mappings) should be declared here.
  */
 public class RobotContainer {
@@ -35,63 +38,50 @@ public class RobotContainer {
 
   // Control del conductor
   private final EngancheSubsystem engancheSubsystem = new EngancheSubsystem();
-  
+
   private final CommandXboxController driverController = new CommandXboxController(
       OperatorConstants.DRIVER_CONTROLLER_PORT);
 
-    // Control del operador
-    private final CommandXboxController operadorController = new CommandXboxController(
+  // Control del operador
+  private final CommandXboxController operadorController = new CommandXboxController(
       OperatorConstants.OPERADOR_CONTROLLER_PORT);
 
-  /** The container for the robot. Contains subsystems, OI devices, and commands. */
+  /**
+   * The container for the robot. Contains subsystems, OI devices, and commands.
+   */
   public RobotContainer() {
     // Configure the trigger bindings
     configureBindings();
   }
 
-  /**
-   * Use this method to define your trigger->command mappings. Triggers can be created via the
-   * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with an arbitrary
-   * predicate, or via the named factories in {@link
-   * edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for {@link
-   * CommandXboxController Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller
-   * PS4} controllers or {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
-   * joysticks}.
-   */
   private void configureBindings() {
 
-  /**
-   * Use this to pass the autonomous command to the main {@link Robot} class.
-   *
-   * @return the command to run in autonomous
-   */
-    
+    driverController.rightBumper()
+        .whileTrue(new AlgaeIntakeCommand(algaeIntakeSubsystem, 0.5));
 
-   driverController.rightBumper()
-   .whileTrue(new AlgaeIntakeCommand(algaeIntakeSubsystem, 0.5));
+    driverController.rightTrigger()
+        .whileTrue(new AlgaeIntakeCommand(algaeIntakeSubsystem, -0.5));
 
-   driverController.rightTrigger()
-   .whileTrue(new AlgaeIntakeCommand(algaeIntakeSubsystem, -0.5));
+    driverController.b()
+        .whileTrue(new EngancheCommand(engancheSubsystem, 0.5));
 
-   driverController.b()
-   .whileTrue(new EngancheCommand(engancheSubsystem, 0.5));
+    driverController.y().onTrue(new AutoDriveCommand(movimientoSubsystem, 10));
 
-   driverController.y().whileTrue(new AutoDriveCommand(movimientoSubsystem, 10));
+    driverController.a().onTrue(new AutoDriveCommand(movimientoSubsystem, 0));
 
-
-   elevadorSubsystem.setDefaultCommand(
+    elevadorSubsystem.setDefaultCommand(
         new ElevadorCommand(
-        elevadorSubsystem,
-    () ->  operadorController.getLeftY()));
+            elevadorSubsystem,
+            () -> operadorController.getLeftY()));
 
-   driverController.x()
-   .whileTrue(new EngancheCommand(engancheSubsystem, -0.5));
+    driverController.x()
+        .whileTrue(new EngancheCommand(engancheSubsystem, -0.5));
 
-  movimientoSubsystem.setDefaultCommand(new MovimientoCommand(
-    () -> -driverController.getLeftY()*0.5,
-    () ->  driverController.getLeftTriggerAxis()*0.5,
-    () -> -driverController.getRightX()*0.5,
-    movimientoSubsystem));
+    movimientoSubsystem.setDefaultCommand(new MovimientoCommand(
+        () -> -driverController.getLeftY() * 0.5,
+        () -> driverController.getLeftTriggerAxis() * 0.5,
+        () -> -driverController.getRightX() * 0.5,
+        movimientoSubsystem));
   }
 
   public Command getAutonomousCommand() {
