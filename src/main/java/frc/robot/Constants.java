60e87847de72ba5d3af3fdfbc48403aa408604d1
Guadalupe Murgo <guadazoemurgo@gmail.com>
2025-02-28 14:24:26 -0300

    Agregue nuevos subsistemas pero en rama basada en main

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -48,4 +48,10 @@ public final class Constants {
     public static final double ElevadorDeadZone = 0.15;
     public static final double MovimientoDeadZone = 0.05;
   }
+
+  public static final class HandConstants {
+    public static final int MUNECA_MOTOR_ID = 9;
+    public static final int ROD_INTERIOR_ID = 10;
+    public static final int ROD_LATERALES_ID = 11;
+  }
 }
--- src/main/java/frc/robot/subsystems/EngancheSubsystem.java
+++ src/main/java/frc/robot/subsystems/EngancheSubsystem.java
@@ -29,4 +29,3 @@ public class EngancheSubsystem extends SubsystemBase {
 
   }
 }
-
