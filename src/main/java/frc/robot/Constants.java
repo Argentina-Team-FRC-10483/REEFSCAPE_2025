239c8e055771668034b26a49f337f32fafe20f1b
Guadalupe Murgo <guadazoemurgo@gmail.com>
2025-03-03 17:28:33 -0300

    Cambie Constants de AlgaeIntakeSubsystem para que tuvieran los mismos valores que el resto de NEOs

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -25,8 +25,6 @@ public final class Constants {
 
   public static final class AlgaeIntakeConstants {
     public static final int RodilloMotor_ID = 7;
-    public static final int RodilloMotor_LIMITE = 60;
-    public static final double RodilloMotor_CompVolt = 10;
     public static final double RodilloMotor_ValorExp = 0.44;
   }
 
--- src/main/java/frc/robot/subsystems/AlgaeIntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/AlgaeIntakeSubsystem.java
@@ -8,6 +8,7 @@ import com.revrobotics.spark.config.SparkMaxConfig;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.AlgaeIntakeConstants;
+import frc.robot.Constants.NEOMotorsConstants;
 
 public class AlgaeIntakeSubsystem extends SubsystemBase {
   private final SparkMax rodilloMotor;
@@ -16,8 +17,8 @@ public class AlgaeIntakeSubsystem extends SubsystemBase {
   public AlgaeIntakeSubsystem() {
     rodilloMotor = new SparkMax(AlgaeIntakeConstants.RodilloMotor_ID, MotorType.kBrushless);
     SparkMaxConfig rodilloMotorConfig = new SparkMaxConfig();
-    rodilloMotorConfig.voltageCompensation(AlgaeIntakeConstants.RodilloMotor_CompVolt);
-    rodilloMotorConfig.smartCurrentLimit(AlgaeIntakeConstants.RodilloMotor_LIMITE);
+    rodilloMotorConfig.voltageCompensation(NEOMotorsConstants.VOLTAGE_COMPENSATION_NEO);
+    rodilloMotorConfig.smartCurrentLimit(NEOMotorsConstants.CURRENT_LIMIT_NEO);
     rodilloMotor.configure(rodilloMotorConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
   }
 
