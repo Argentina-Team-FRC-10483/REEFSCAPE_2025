dc4e612306a48721ffa5bd54e8f4b07ed058b90a
Joaquín Figueroa <ijfigueroa2007@gmail.com>
2025-03-05 17:40:19 -0300

    Delete unused deploys and fix autochooser

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -7,6 +7,7 @@ package frc.robot;
 import com.pathplanner.lib.auto.AutoBuilder;
 import com.pathplanner.lib.auto.NamedCommands;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
@@ -62,6 +63,7 @@ public class RobotContainer {
     NamedCommands.registerCommand("SubirElevador", new ElevatorToL4Command(elevatorSubsystem));
 
     configureBindings();
+    SmartDashboard.putData("Auto Chooser", autoChooser);
   }
 
   /**
