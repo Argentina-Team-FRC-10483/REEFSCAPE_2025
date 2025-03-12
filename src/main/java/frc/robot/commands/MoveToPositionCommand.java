6c661d679a7492472e2b29b3b9623edd68ac0ba5
Diego Avila <aviladiego2009@gmail.com>
2025-03-12 17:12:32 -0300

    Se añadió MoveToPositionCommand a una nueva rama

---
+++ src/main/java/frc/robot/commands/MoveToPositionCommand.java
@@ -0,0 +1,26 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.MovableSubsystem;
+
+public class MoveToPositionCommand extends Command {
+  protected final MovableSubsystem subsystem;
+  double targetPosition;
+  double tolerance;
+
+  public MoveToPositionCommand(double targetPosition, MovableSubsystem subsystem, double tolerance) {
+    this.targetPosition = targetPosition;
+    this.subsystem = subsystem;
+    this.tolerance = tolerance;
+  }
+
+  @Override
+  public void execute() {
+    subsystem.move(targetPosition);
+  }
+
+  @Override
+  public boolean isFinished() {
+    return Math.abs(subsystem.getActualPosition() - targetPosition) < tolerance;
+  }
+}
